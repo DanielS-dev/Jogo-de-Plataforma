75daf3281f38e436c90a5b8b414958445243f606
Daniel Silva <daniel_santos2002@outlook.com>
2021-02-04 14:25:56 -0200

    Aula 05 - Monstro

---
+++ Monstro.gd
@@ -0,0 +1,26 @@
+extends StaticBody2D
+
+var flip = true
+var posicao_inicial
+var posicao_final
+var velocidade = 0.3
+
+func _ready():
+	$Sprite.play("Walk")
+	posicao_inicial = $".".position.x
+	posicao_final = posicao_inicial + 100
+	pass
+
+func _process(delta):
+	
+	if (flip):
+		$".".position.x += velocidade
+		$Sprite.flip_h = false
+		if ($".".position.x >= posicao_final):
+			flip = false
+	if (!flip):
+		$Sprite.flip_h = true
+		$".".position.x -= velocidade
+		if ($".".position.x <= posicao_inicial):
+			flip = true
+	pass
